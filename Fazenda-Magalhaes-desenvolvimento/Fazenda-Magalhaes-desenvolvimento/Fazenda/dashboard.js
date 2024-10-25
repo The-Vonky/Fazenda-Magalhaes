83bff3a3c3b723167de1b1d8d33ae00602b49ad9
Vonky <deywidbraga14@gmail.com>
2024-10-25 17:44:03 -0300

    Grandes mudanças em dashboard, modelo quase pronto

--- Fazenda-Magalhaes-desenvolvimento/Fazenda-Magalhaes-desenvolvimento/Fazenda/dashboard.js
+++ Fazenda-Magalhaes-desenvolvimento/Fazenda-Magalhaes-desenvolvimento/Fazenda/dashboard.js
@@ -1,121 +1,36 @@
-// SIDEBAR TOGGLE
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
 
-let sidebarOpen = false;
-const sidebar = document.getElementById('sidebar');
+// Show sidebar
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
 
-function openSidebar() {
-  if (!sidebarOpen) {
-    sidebar.classList.add('sidebar-responsive');
-    sidebarOpen = true;
-  }
-}
+// Close sidebar
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
 
-function closeSidebar() {
-  if (sidebarOpen) {
-    sidebar.classList.remove('sidebar-responsive');
-    sidebarOpen = false;
-  }
-}
+// Change theme
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
 
-// ---------- CHARTS ----------
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+});
 
-// BAR CHART
-const barChartOptions = {
-  series: [
-    {
-      data: [4130, 5778, 4293, 5109, 5707, 4676, 4394],
-    },
-  ],
-  chart: {
-    type: 'bar',
-    height: 350,
-    toolbar: {
-      show: false,
-    },
-  },
-  colors: ['#246dec', '#cc3c43', '#367952', '#f5b74f', '#4f35a1'],
-  plotOptions: {
-    bar: {
-      distributed: true,
-      borderRadius: 4,
-      horizontal: false,
-      columnWidth: '40%',
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  legend: {
-    show: false,
-  },
-  xaxis: {
-    categories: [
-      'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-  },
-  yaxis: {
-    title: {
-      text: 'Litros',
-    },
-  },
-};
-
-const barChart = new ApexCharts(
-  document.querySelector('#bar-chart'),
-  barChartOptions
-);
-barChart.render();
-
-// AREA CHART
-const areaChartOptions = {
-  series: [
-    {
-      name: 'Litros',
-      data: [5161, 4788, 5534, 4284, 5461, 4621, 5318 ],
-    },
-    {
-      name: 'Total',
-      data: [4130, 5778, 4293, 5109, 5707, 4676, 4394],
-    },
-  ],
-  chart: {
-    height: 350,
-    type: 'area',
-    toolbar: {
-      show: false,
-    },
-  },
-  colors: ['#4f35a1', '#246dec'],
-  dataLabels: {
-    enabled: false,
-  },
-  stroke: {
-    curve: 'smooth',
-  },
-  labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-  markers: {
-    size: 0,
-  },
-  yaxis: [
-    {
-      title: {
-        text: 'Litros',
-      },
-    },
-    {
-      opposite: true,
-      title: {
-        text: 'Litros',
-      },
-    },
-  ],
-  tooltip: {
-    shared: true,
-    intersect: false,
-  },
-};
-
-const areaChart = new ApexCharts(
-  document.querySelector('#area-chart'),
-  areaChartOptions
-);
-areaChart.render();
+// Fill orders in table
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.paymentStatus}</td>
+                        <td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning' : 'primary'}">${order.shipping}</td>
+                        <td class="primary">Detalhes</td>
+                        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+});
