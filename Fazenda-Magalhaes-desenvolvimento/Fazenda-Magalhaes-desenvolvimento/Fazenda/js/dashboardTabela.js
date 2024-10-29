c2bf60041b708fe8c276c6b1323d088555c77dae
Vonky <deywidbraga14@gmail.com>
2024-10-29 17:25:15 -0300

    organização de pastas, teste de caroussel

---
+++ Fazenda-Magalhaes-desenvolvimento/Fazenda-Magalhaes-desenvolvimento/Fazenda/js/dashboardTabela.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Mimosa',
+        productNumber: '85631',
+        paymentStatus: '3',
+        shipping: 'Afastada'
+    },
+    {
+        productName: 'Mimosa',
+        productNumber: '85631',
+        paymentStatus: '3',
+        shipping: 'Afastada'
+    },
+    {
+        productName: 'Mimosa',
+        productNumber: '85631',
+        paymentStatus: '3',
+        shipping: 'Afastada'
+    },
+    {
+        productName: 'Mimosa',
+        productNumber: '85631',
+        paymentStatus: '3',
+        shipping: 'Afastada'
+    },
+    {
+        productName: 'Mimosa',
+        productNumber: '85631',
+        paymentStatus: '3',
+        shipping: 'Afastada'
+    },
+    {
+        productName: 'Mimosa',
+        productNumber: '85631',
+        paymentStatus: '3',
+        shipping: 'Afastada'
+    }
+]
